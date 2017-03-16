ca2a398ccb7d6b5ad5632a592d349c81ec4d2f26
Sandipan Dutta <Sandipan Dutta>
2017-03-16 20:30:15 +0530

    Still Trying

--- myproj/wsgi.py
+++ myproj/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeedapp.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproj.settings")
-
-application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(application)
