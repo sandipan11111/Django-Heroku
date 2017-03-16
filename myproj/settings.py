5fc44b3a463bdc721b26f54d9a26349f454f2e82
Sandipan Dutta <Sandipan Dutta>
2017-03-16 20:06:00 +0530

    Trying

--- myproj/settings.py
+++ myproj/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+STATIC_URL = '/static/'
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
--- myproj/wsgi.py
+++ myproj/wsgi.py
@@ -16,4 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproj.settings")
 application = get_wsgi_application()
 
 from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
 application = DjangoWhiteNoise(application)
